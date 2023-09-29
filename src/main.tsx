867f6b0db8e05db9a3b19555d7096effe938814f
Ivan Magaš <imagas96@gmail.com>
2023-09-29 11:21:16 +0200

    beam config

--- src/main.tsx
+++ src/main.tsx
@@ -21,7 +21,84 @@ import App from "./App";
 import { WagmiConfig, configureChains, createClient } from "wagmi";
 
 const { chains, provider } = configureChains(
-  [mainnet, goerli, polygon, polygonMumbai, avalanche, avalancheFuji],
+  [mainnet, goerli, polygon, polygonMumbai, avalanche, avalancheFuji,
+    {
+      id: 13_337,
+      name: 'Beam Testnet',
+      network: 'Beam Testnet',
+      nativeCurrency: {
+        decimals: 18,
+        name: 'Merit Circle',
+        symbol: 'MC',
+        logoUri:
+          'https://images.ctfassets.net/gcj8jwzm6086/488HFZvUO1qpnYRA9PDHCo/5664b04bc1de6341ed7c480c79e1c8de/token-logo.png',
+      },
+      rpcUrls: {
+        public: {
+          http: ['https://subnets.avax.network/beam/testnet/rpc'],
+        },
+        default: {
+          http: ['https://subnets.avax.network/beam/testnet/rpc'],
+        },
+      },
+      blockExplorers: {
+        default: {
+          name: 'Explorer',
+          url: 'https://subnets-test.avax.network/beam',
+        },
+      },
+      contracts: {
+        multicall3: {
+          address: '0x9BF49b704EE2A095b95c1f2D4EB9010510c41C9E',
+          blockCreated: 3,
+        },
+        weth: {
+          address: '0xF65B6f9c94187276C7d91F4F74134751d248bFeA',
+        },
+        uniswapV2Router: {
+          address: '0xB4cFBc4836c5a0Eb27A502B6008f9baF3Bf8b3Ee',
+        },
+      },
+      testnet: true,
+    },
+    {
+      id: 4337,
+      name: 'Beam Mainnet',
+      network: 'Beam',
+      nativeCurrency: {
+        decimals: 18,
+        name: 'Merit Circle',
+        symbol: 'MC',
+        logoUri:
+          'https://images.ctfassets.net/gcj8jwzm6086/488HFZvUO1qpnYRA9PDHCo/5664b04bc1de6341ed7c480c79e1c8de/token-logo.png',
+      },
+      rpcUrls: {
+        public: {
+          http: ['https://subnets.avax.network/beam/mainnet/rpc'],
+        },
+        default: {
+          http: ['https://subnets.avax.network/beam/mainnet/rpc'],
+        },
+      },
+      blockExplorers: {
+        default: {
+          name: 'Explorer',
+          url: 'https://subnets.avax.network/beam',
+        },
+      },
+      contracts: {
+        multicall3: {
+          address: '0x4956f15efdc3dc16645e90cc356eafa65ffc65ec',
+          blockCreated: 3,
+        },
+        weth: {
+          address: '0xD51BFa777609213A653a2CD067c9A0132a2D316A',
+        },
+        uniswapV2Router: {
+          address: '0x965B104e250648d01d4B3b72BaC751Cde809D29E',
+        },
+      },
+    }],
   [
     publicProvider({
       priority: 1,
